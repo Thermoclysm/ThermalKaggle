edb44708a72c7b5eccc2a28a0b70d19ebdc1801d
SFbwt <powdercrash16@gmail.com>
2018-10-21 20:35:11 +0100

    Initial test fit
    Added basic logistic function fitting

--- WalkingDistance.py
+++ WalkingDistance.py
@@ -9,7 +9,8 @@ Looking at win percentage as a function of walking distance
 
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from scipy.optimize import curve_fit
+import numpy as np
 #%%
 
 df0 = pd.read_csv("datasets/train_V2.csv")
@@ -55,19 +56,36 @@ plt.errorbar(bin_edges[:-2], mu1, yerr=sige1)
 #%%
 """restrict to solo fpp games only"""
 
-dfsolofpp = df0[df0['matchType']=='solo-fpp']
+dfsf = df0[df0['matchType']=='solo-fpp']
 mu2=[]
 sig2 =[]
 sige2 = []
-bin_walks(dfsolofpp, mu2, sig2, sige2)
+bin_walks(dfsf, mu2, sig2, sige2)
 
 plt.figure(3)
 plt.title("Average win percentage for binned walking distances")
-plt.scatter(bin_edges[:-2],mu1)
+plt.scatter(bin_edges[:-2],mu1, label='all matches')
 plt.ylim(ymin=0,ymax=1.25)
-plt.errorbar(bin_edges[:-2], mu1, yerr=sige1)
-plt.scatter(bin_edges[:-2],mu2)
+plt.errorbar(bin_edges[:-2], mu1, yerr=sige1, marker='x')
+plt.scatter(bin_edges[:-2],mu2, label='solo only', marker='+')
 plt.ylim(ymin=0,ymax=1.25)
 plt.errorbar(bin_edges[:-2], mu2, yerr=sige2)
+plt.legend(loc='best')
+
+
+#%%
+"""initial fit test using logistic function"""
+
+def log4p(x, x0, L, k):
+    return L/(1+np.exp(-k*(x-x0)))
+    
+xtest = np.linspace(0, 10000, 1000)
+ytest = log4p(xtest, 2200, 1, 0.5)
+    
+popt, pcov = curve_fit(log4p, bin_edges[:-2], mu1, bounds=((1000,0.8,0),(2500,0.9,0.05)))
+popt
+
+plt.plot(bin_edges[:-2], log4p(bin_edges[:-2], *popt), 'r--',
+         label='fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
 
     
